1d8bcc0a7fc319909c1266efaf68c4f1599cbcae
CitrusMaxima <2663753900@qq.com>
2019-03-31 14:14:54 +0800

    通用页面标题的开发

---
+++ src/component/page-title/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Layout extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        document.title = this.props.title + ' - MMALL';
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="page-header">{this.props.title}</h1>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Layout;
