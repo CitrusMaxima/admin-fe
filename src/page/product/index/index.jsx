7ad591bdd66d261851e88ce57bbac1e09ff29f2d
CitrusMaxima <2663753900@qq.com>
2019-04-02 16:52:20 +0800

    商品列表的开发

--- src/page/product/index/index.jsx
+++ src/page/product/index/index.jsx
@@ -7,6 +7,8 @@ import PageTitle from 'component/page-title/index.jsx';
 import TableList from 'util/table-list/index.jsx';
 import Pagination from 'util/pagination/index.jsx';
 
+import './index.scss';
+
 const _mm = new MUtil();
 const _product = new Product();
 
@@ -43,20 +45,57 @@ class ProductList extends React.Component {
         });
     }
 
+    // 改变商品状态，上架 / 下架
+    onSetProductStatus(e, productId, currentStatus) {
+        let newStatus = currentStatus == 1 ? 2 : 1,
+            confirmTips = currentStatus == 1 ? '确定要下架该商品？' : '确定要上架该商品？';
+        if (window.confirm(confirmTips)) {
+            _product.setProductStatus({
+                productId: productId,
+                status: newStatus
+            }).then(res => {
+                _mm.successTips(res);
+                this.loadProductList();
+            }, errMsg => {
+                _mm.errorTips(errMsg);
+            });
+        }
+    }
+
     render() {
+        let tableHeads = [
+            {name: '商品ID', width: '10%'},
+            {name: '商品信息', width: '50%'},
+            {name: '价格', width: '10%'},
+            {name: '状态', width: '15%'},
+            {name: '操作', width: '15%'}
+        ];
         return (
             <div id="page-wrapper">
                 <PageTitle title="商品列表"/>
-                <TableList tableHeads={['ID', '用户名', '邮箱', '电话', '注册时间']}>
+                <TableList tableHeads={tableHeads}>
                     {
-                        this.state.list.map((user, index) => {
+                        this.state.list.map((product, index) => {
                             return (
                                 <tr key={index}>
-                                    <td>{user.id}</td>
-                                    <td>{user.username}</td>
-                                    <td>{user.email}</td>
-                                    <td>{user.phone}</td>
-                                    <td>{new Date(user.createTime).toLocaleString()}</td>
+                                    <td>{product.id}</td>
+                                    <td>
+                                        <p>{product.name}</p>
+                                        <p>{product.subtitle}</p>
+                                    </td>
+                                    <td>￥{product.price}</td>
+                                    <td>
+                                        <span>{product.status == 1 ? '在售' : '已下架'}</span>
+                                        <button className="btn btn-xs btn-warning" onClick={(e) => {
+                                            this.onSetProductStatus(e, product.id, product.status)
+                                        }}>
+                                            {product.status == 1 ? '下架' : '上架'}
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <Link className="opera" to={`/product/detail/${product.id}`}>查看</Link>
+                                        <Link className="opera" to={`/product/save/${product.id}`}>编辑</Link>
+                                    </td>
                                 </tr>
                             );
                         })
